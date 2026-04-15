a9cc8b8203a04d71809335492ff849acd6102716
iamtomij <helloiamtomij@gmail.com>
2026-04-15 17:00:08 +0600

    Login functionality

---
+++ JS/login.js
@@ -0,0 +1,19 @@
+const loginBtn = document.getElementById("btn-login");
+
+loginBtn.addEventListener("click", function () {
+   const username = document.getElementById("username").value;
+   const password = document.getElementById("password").value;
+
+   // demo credentials
+   if (username === "admin" && password === "admin123") {
+
+      // save login state
+      localStorage.setItem("isLoggedIn", "true");
+
+      // redirect to main page
+      window.location.href = "index.html";
+
+   } else {
+      alert("Invalid username or password");
+   }
+});
